02758f5bc04117fe26fd8a9c74231f18a6474e54
Breno Alves <106405847+Balves129@users.noreply.github.com>
2024-05-06 18:49:59 -0300

    first commit

---
+++ src/conexaoDb/ConexaoDb.java
@@ -0,0 +1,32 @@
+package conexaoDb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoDb {
+
+    private static final String url = "jdbc:mysql://localhost:3306/projeto1";
+    private static final String user = "root";
+    private static final String password = "q1w2e3";
+
+    private static Connection conn;
+
+    public static Connection getConnection() {
+
+        try{
+            if(conn == null){
+                conn = DriverManager.getConnection(url, user, password);
+                System.out.println("Conectado com sucesso!");
+                return conn;
+            } else{
+                return conn;
+            }
+        } catch (SQLException e) {
+
+                e.printStackTrace();
+                return null;
+            }
+            
+        }
+};
